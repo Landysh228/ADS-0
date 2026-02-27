22e846f003c742bdd8432a90a7360389bf005322
Landysh <walfa45milana@mail.ru>
2026-02-28 00:48:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,10 @@
-int gcd(int a = 25, int b = 10) {
-	a = a < 0 ? -a : a;
-    b = b < 0 ? -b : b;
-    
-    if (b == 0) {
-        return a;
+int gcd(int a, int b) {
+	a = abs(a);
+    b = abs(b);
+    while (b != 0) {
+        int remainder = a % b;
+        a = b;
+        b = remainder;
     }
-    return gcd(b, a % b);
+    return a;
 }
