d698550369d7a224a841fae567b29b736430fb89
Landysh <walfa45milana@mail.ru>
2026-02-27 19:52:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,8 @@
-// Copyright 2021 NNTU-CS
-
-int gcd(int a, int b) {
+int gcd(int a, int b){
 	a = abs(a);
     b = abs(b);
-    
     if (b == 0) {
         return a;
     }
     return gcd(b, a % b);
-}
+} 
