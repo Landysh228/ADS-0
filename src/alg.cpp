092a634d2d1c9d1c61455114457e6295c3cfa07d
Landysh <walfa45milana@mail.ru>
2026-02-28 11:03:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,15 @@
+#include <iostream>
+#include "alg.h"
 int gcd(int a, int b) {
-	while (a != 0 && b != 0) {
-        if (a > b) {
-            a = a % b;
-        } else {
-            b = b % a;
-        }
+    while (b != 0) {
+        int i = b;
+        b = a % b;
+        a = i;
     }
-    return a + b;
+    return a;
+}
+
+int main() {
+    std::cout << gcd(10, 15) << std::endl;
+    return 0;
 }
